a8e00ce4c206918df1edfe20a7a67b286be341d3
Will Strickland <wstrickland@smu.edu>
2021-12-26 17:48:35 -0600

    Initial Commit

---
+++ Ortis/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ortis
+//
+//  Created by William Strickland on 12/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ortis/OrtisApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrtisApp.swift
+//  Ortis
+//
+//  Created by William Strickland on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct OrtisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
