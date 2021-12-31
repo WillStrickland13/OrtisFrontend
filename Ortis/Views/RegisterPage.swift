c884361906cdcbbdfec7925032383f77844a7d62
Will Strickland <wstrickland@smu.edu>
2021-12-30 22:44:15 -0600

    added profile and register pages

---
+++ Ortis/Views/RegisterPage.swift
@@ -0,0 +1,23 @@
+//
+//  RegisterPage.swift
+//  Ortis
+//
+//  Created by William Strickland on 12/30/21.
+//
+
+import SwiftUI
+
+struct RegisterPage: View {
+    var body: some View {
+        VStack{
+            Image("Logo").resizable()
+                .frame(width: 75, height: 75)
+        }
+    }
+}
+
+struct RegisterPage_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterPage()
+    }
+}
