5e79d6e776fb5c2739015d79f943454370f37a9d
Will Strickland <wstrickland@smu.edu>
2022-01-02 15:24:23 -0600

    Sign up button now posts new user
    need to add functionality to go to different page

--- Ortis/Views/SignUpView.swift
+++ Ortis/Views/SignUpView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-
+import Alamofire
 struct SignUpView: View {
     @State var firstName: String = ""
     @State var lastName: String = ""
@@ -16,10 +16,11 @@ struct SignUpView: View {
     var body: some View {
         VStack{
             Image("Logo").resizable()
-                .frame(width: 100, height: 100, alignment: .center)
-            Text("Sign up")
-                .fontWeight(.semibold)
-                .font(.largeTitle)
+                .padding()
+                .frame(width: 125, height: 125, alignment: .center)
+//            Text("Sign up")
+//                .fontWeight(.semibold)
+//                .font(.largeTitle)
             HStack{
             TextField("First Name", text: $firstName)
                 .padding()
@@ -32,11 +33,13 @@ struct SignUpView: View {
                 .cornerRadius(5.0)
                 .padding(.bottom, 3)
             }
+            .padding(.top)
             TextField("Username", text: $username)
                 .padding()
                 .background(Color.gray)
                 .cornerRadius(5.0)
                 .padding(.bottom, 3)
+            
             TextField("Email", text: $email)
                 .padding()
                 .background(Color.gray)
@@ -47,9 +50,42 @@ struct SignUpView: View {
                 .background(Color.gray)
                 .cornerRadius(5.0)
                 .padding(.bottom, 3)
+            ZStack{
+                Capsule()
+                    .fill(Color("SecondaryColor"))
+                    .frame(width: 160, height: 60, alignment: .center)
+            
+            
+                Text("Sign Up")
+                    .fontWeight(.semibold)
+                    .font(.largeTitle)
+                    .foregroundColor(Color(red: 1.0, green: 0.0,        blue: 0.0, opacity: 1.0))
+                    .padding()
+                
+                
+            
+            }.padding()
+                .onTapGesture {
+                    print(firstName,lastName,username,email,password)
+                    postUser(first:firstName,last:lastName,user:username,em:email,pswd:password)
+                }
         }.padding()
             
     }
+}
+
+func postUser(first:String, last:String, user:String, em:String, pswd:String){
+    let params:Parameters=[
+        "firstName":first,
+        "lastName":last,
+        "username":user,
+        "email":em,
+        "password":pswd
+    ]
+
+    AF.request("http://0.0.0.0:8000/users/register", method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil).validate(statusCode: 200 ..< 299).responseJSON { AFdata in
+            
+        }
 }
 
 struct SignUpView_Previews: PreviewProvider {
